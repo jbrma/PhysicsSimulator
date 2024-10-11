dd5022f0914a43da2cbfc0c55b68e47541817d80
Jorge <91422873+jbrma@users.noreply.github.com>
2024-10-11 13:21:29 +0200

    Delete src directory

--- src/simulator/model/DequeuingStrategy.java
+++
@@ -1,9 +0,0 @@
-package simulator.model;
-
-import java.util.*;
-
-public interface DequeuingStrategy {
-	
-	List<Vehicle> dequeue(List<Vehicle> q);
-
-}
--- src/simulator/model/LightSwitchingStrategy.java
+++
@@ -1,9 +0,0 @@
-package simulator.model;
-
-import java.util.*;
-
-public interface LightSwitchingStrategy {
-
-	int chooseNextGreen(List<Road> roads, List<List<Vehicle>> qs, int currGreen, int lastSwitchingTime, int currTime);
-
-}
